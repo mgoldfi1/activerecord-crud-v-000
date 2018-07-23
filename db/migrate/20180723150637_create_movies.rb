097a1fbcd38db515372d94514b113953ec3fa05f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 15:09:01 +0000

    automatically backed up by learn

--- db/migrate/20180723150637_create_movies.rb
+++ db/migrate/20180723150637_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |x|
       x.string :title
-      x.string :release_date
+      x.integer :release_date
       x.string :director
       x.string :lead
       x.boolean :in_theaters
