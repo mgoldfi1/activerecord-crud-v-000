05ace2614626579891a54c1b66ae931e3b584159
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 16:49:17 +0000

    Done.

---
+++ db/migrate/20180723150637_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |x|
+      x.string :title
+      x.integer :release_date
+      x.string :director
+      x.string :lead
+      x.boolean :in_theaters
+  end
+end
+
+end
