16973245d2d18761b23322d3cae4cd8cc3597286
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 16:24:00 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,11 +33,11 @@ def can_be_created_in_a_block(args = {})
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.all.first
+  Movie.all.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.all.last.title
 end
 
 def can_get_size_of_the_database
