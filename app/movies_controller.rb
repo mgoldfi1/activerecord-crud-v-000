4cde2fbf29a90df87563d2cb7c924b1c8ba33b3b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 16:46:31 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,9 +79,10 @@ def can_update_using_update_method
 end
 
 def can_update_multiple_items_at_once
-  # Change title of all movies to "A Movie"
-  # 5.times do |i|
-  #   Movie.create(title: "Movie_#{i}", release_date: 2000+i)
+  #  Change title of all movies to "A Movie"
+   5.times do |i|
+     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
+   end
   Movie.all.each do |x|
     x.update(title: "A Movie")
   end
