bcdf77f2689a81c254f055377088ea102baf3762
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 16:20:34 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args)
+def can_be_created_in_a_block(args = {})
   binding.pry
   Movie.create do |m|
     m.title = args[:title]
